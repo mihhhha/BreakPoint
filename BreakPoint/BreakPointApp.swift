cf30cf5c5cc9b1b4e6b1509e1b38712555ef2ffe
Mykhailo Poliakov <mihhhha@gmail.com>
2023-11-06 12:10:14 +0100

    Initial Commit

---
+++ BreakPoint/BreakPointApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakPointApp.swift
+//  BreakPoint
+//
+//  Created by mihhhha on 06/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct BreakPointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
